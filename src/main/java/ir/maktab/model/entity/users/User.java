a8841d3f475acf84055a32c8b65b89e6df25c0e9
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 01:05:18 +0330

    add hibernate annotations to User.java

--- src/main/java/ir/maktab/model/entity/users/User.java
+++ src/main/java/ir/maktab/model/entity/users/User.java
@@ -1,16 +1,25 @@
 package ir.maktab.model.entity.users;
 
 import ir.maktab.model.enumeration.UserStatus;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@MappedSuperclass
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstname;
     private String lastname;
     private String emailAddress;
     private String password;
+    @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
+    @CreationTimestamp
     private Date registrationDate;
     private Long credit;
 }
