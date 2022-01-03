e13dfb8c2a43e31c1ce28e79b762bcfda9671267
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:23:36 +0330

    add expertDao property to ExpertService

--- src/main/java/ir/maktab/service/ExpertService.java
+++ src/main/java/ir/maktab/service/ExpertService.java
@@ -1,4 +1,11 @@
 package ir.maktab.service;
 
+import ir.maktab.dao.ExpertDao;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ExpertService {
+    private ExpertDao expertDao;
 }
