476643fbb0875ca6379ac42dbdf04c904f51dbce
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:54:59 +0330

    add save method to MainServiceService

--- src/main/java/ir/maktab/service/MainServiceService.java
+++ src/main/java/ir/maktab/service/MainServiceService.java
@@ -1,6 +1,7 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.MainServiceDao;
+import ir.maktab.model.entity.services.MainService;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,4 +9,8 @@ import lombok.Setter;
 @Setter
 public class MainServiceService {
     private MainServiceDao mainServiceDao;
+
+    public void save(MainService mainService) {
+        mainServiceDao.save(mainService);
+    }
 }
