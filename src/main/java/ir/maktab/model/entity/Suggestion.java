50760cfda19605bc8974dd44f85bff203db98a5b
nasim zamani <zamaninasim213@gmail.com>
2022-01-03 23:49:50 +0330

    add Suggestion.java

--- src/main/java/ir/maktab/model/entity/Suggestion.java
+++ src/main/java/ir/maktab/model/entity/Suggestion.java
@@ -1,4 +1,14 @@
 package ir.maktab.model.entity;
 
+import ir.maktab.model.entity.users.Expert;
+
+import java.util.Date;
+
 public class Suggestion {
+    private Integer id;
+    private Expert expert;
+    private Date registrationDate;
+    private Long proposedPrice;
+    private int durationOfWork;
+    private Date startTime;
 }
