3e80fb5204385da5de87c49aaee733100e8ed63f
nasim zamani <zamaninasim213@gmail.com>
2022-01-03 22:20:14 +0330

    adding Property to SubService.java

--- src/main/java/ir/maktab/model/entity/services/SubService.java
+++ src/main/java/ir/maktab/model/entity/services/SubService.java
@@ -3,5 +3,7 @@ package ir.maktab.model.entity.services;
 public class SubService {
     private Integer id;
     private String name;
+    private Long basePrice;
+    private String description;
     private MainService mainService;
 }
