e0a806a7758984388d62a1d198adaef70766e714
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:24:46 +0330

    add mainServiceDao property to MainServiceService

--- src/main/java/ir/maktab/service/MainServiceService.java
+++ src/main/java/ir/maktab/service/MainServiceService.java
@@ -1,4 +1,11 @@
 package ir.maktab.service;
 
+import ir.maktab.dao.MainServiceDao;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MainServiceService {
+    private MainServiceDao mainServiceDao;
 }
