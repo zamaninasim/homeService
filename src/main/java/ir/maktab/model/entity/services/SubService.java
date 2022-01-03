8671f25d1f872ffe8220e36db3ba78a277248070
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 01:00:00 +0330

    add hibernate annotations to SubService.java

--- src/main/java/ir/maktab/model/entity/services/SubService.java
+++ src/main/java/ir/maktab/model/entity/services/SubService.java
@@ -1,9 +1,19 @@
 package ir.maktab.model.entity.services;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class SubService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private Long basePrice;
+    @Lob
     private String description;
+    @ManyToOne
     private MainService mainService;
 }
