eedb80df3680655388758047569ba5da3ff5b22a
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 01:11:38 +0330

    add hibernate annotations to Expert.java

--- src/main/java/ir/maktab/model/entity/users/Expert.java
+++ src/main/java/ir/maktab/model/entity/users/Expert.java
@@ -1,11 +1,20 @@
 package ir.maktab.model.entity.users;
 
 import ir.maktab.model.entity.services.SubService;
+import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import java.util.Set;
-
+@Data
+@Entity
 public class Expert extends User {
+    @Lob
+    @Column(columnDefinition="BLOB")
     private byte[] photo;
-    private double score;
+    private Double score;
+    @ManyToMany
     private Set<SubService> services;
 }
