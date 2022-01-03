3e4950dbab075a0589b81f1ae6b7acf7696dd62b
nasim zamani <zamaninasim213@gmail.com>
2022-01-03 22:17:24 +0330

    add MainService.java & SubService.java

---
+++ src/main/java/ir/maktab/model/entity/services/MainService.java
@@ -0,0 +1,9 @@
+package ir.maktab.model.entity.services;
+
+import java.util.Set;
+
+public class MainService {
+    private Integer id;
+    private String name;
+    private Set<SubService> subServices;
+}
---
+++ src/main/java/ir/maktab/model/entity/services/SubService.java
@@ -0,0 +1,7 @@
+package ir.maktab.model.entity.services;
+
+public class SubService {
+    private Integer id;
+    private String name;
+    private MainService mainService;
+}
