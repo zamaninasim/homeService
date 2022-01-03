2e3620f2ff49d376d5fc656dd1ee45ac0f79cca7
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:54:14 +0330

    add save method to MainServiceDao

--- src/main/java/ir/maktab/dao/MainServiceDao.java
+++ src/main/java/ir/maktab/dao/MainServiceDao.java
@@ -1,5 +1,19 @@
 package ir.maktab.dao;
 
-public class MainServiceDao {
+import ir.maktab.model.entity.services.MainService;
+import ir.maktab.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+public class MainServiceDao {
+    private SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+
+    public void save(MainService mainService) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(mainService);
+        transaction.commit();
+        session.close();
+    }
 }
--- src/main/java/ir/maktab/view/Main.java
+++ src/main/java/ir/maktab/view/Main.java
@@ -1,4 +1,13 @@
 package ir.maktab.view;
 
+import ir.maktab.config.ServiceConfig;
+import ir.maktab.service.MainServiceService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ServiceConfig.class);
+        MainServiceService mainServiceService = context.getBean(MainServiceService.class);
+    }
 }
