bb40d0b0b8d6957e22ee4febeeaffc18a04cbdfe
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:25:54 +0330

    add orderDao property to OrderService

--- src/main/java/ir/maktab/service/OrderService.java
+++ src/main/java/ir/maktab/service/OrderService.java
@@ -1,4 +1,11 @@
 package ir.maktab.service;
 
+import ir.maktab.dao.OrderDao;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class OrderService {
+    private OrderDao orderDao;
 }
