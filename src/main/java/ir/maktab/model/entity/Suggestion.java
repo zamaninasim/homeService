7213aee5456118b7e2ae40325e982111611da8a4
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 01:20:35 +0330

    add hibernate annotations to Suggestion.java

--- src/main/java/ir/maktab/model/entity/Suggestion.java
+++ src/main/java/ir/maktab/model/entity/Suggestion.java
@@ -1,14 +1,23 @@
 package ir.maktab.model.entity;
 
 import ir.maktab.model.entity.users.Expert;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
 import java.util.Date;
-
+@Entity
+@Data
 public class Suggestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne
     private Expert expert;
+    @CreationTimestamp
     private Date registrationDate;
     private Long proposedPrice;
     private int durationOfWork;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date startTime;
 }
