16abc8c8adb323bcae9b60230dfe38f68fe652e8
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:20:33 +0330

    add commentDao property to CommentService

--- src/main/java/ir/maktab/service/CommentService.java
+++ src/main/java/ir/maktab/service/CommentService.java
@@ -1,4 +1,11 @@
 package ir.maktab.service;
 
+import ir.maktab.dao.CommentDao;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CommentService {
+    private CommentDao commentDao;
 }
