bcc29124fda2e5c7a3e78c5e66d164e8216ff330
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:22:37 +0330

    add getter & setter to CustomerService

--- src/main/java/ir/maktab/service/CustomerService.java
+++ src/main/java/ir/maktab/service/CustomerService.java
@@ -1,7 +1,11 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.CustomerDao;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CustomerService {
     private CustomerDao customerDao;
 }
