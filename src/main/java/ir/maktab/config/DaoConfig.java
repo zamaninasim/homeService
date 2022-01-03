1c005768e6cdb5af50e9a479baf779e3a64ad822
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:14:25 +0330

    add Dao spring config

--- src/main/java/ir/maktab/config/DaoConfig.java
+++ src/main/java/ir/maktab/config/DaoConfig.java
@@ -1,6 +1,7 @@
 package ir.maktab.config;
 
-import ir.maktab.dao.MainServiceDao;
+import ir.maktab.dao.*;
+import ir.maktab.model.entity.services.SubService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +10,35 @@ public class DaoConfig {
     @Bean
     public MainServiceDao mainServiceDao() {
         return new MainServiceDao();
+    }
+
+    @Bean
+    public CommentDao commentDao() {
+        return new CommentDao();
+    }
+
+    @Bean
+    public CustomerDao customerDao() {
+        return new CustomerDao();
+    }
+
+    @Bean
+    public ExpertDao expertDao() {
+        return new ExpertDao();
+    }
+
+    @Bean
+    public OrderDao orderDao() {
+        return new OrderDao();
+    }
+
+    @Bean
+    public SubService subService() {
+        return new SubService();
+    }
+
+    @Bean
+    public SuggestionDao suggestionDao() {
+        return new SuggestionDao();
     }
 }
