a8ee3de5ea2ba8cd226eaa217c2b2c5a16f08cb0
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 02:33:07 +0330

    add customerService bean

--- src/main/java/ir/maktab/config/ServiceConfig.java
+++ src/main/java/ir/maktab/config/ServiceConfig.java
@@ -1,7 +1,9 @@
 package ir.maktab.config;
 
 import ir.maktab.dao.CommentDao;
+import ir.maktab.dao.CustomerDao;
 import ir.maktab.service.CommentService;
+import ir.maktab.service.CustomerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -15,4 +17,12 @@ public class ServiceConfig {
         commentService.setCommentDao(commentDao);
         return commentService;
     }
+
+    @Bean
+    public CustomerService customerService(CustomerDao customerDao) {
+        CustomerService customerService = new CustomerService();
+        customerService.setCustomerDao(customerDao);
+        return customerService;
+    }
+
 }
