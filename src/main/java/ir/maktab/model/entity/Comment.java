c77ef0c461dac9a6ffd75e7a7e98c9c9178a0467
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 00:46:13 +0330

    add property to Comment

--- src/main/java/ir/maktab/model/entity/Comment.java
+++ src/main/java/ir/maktab/model/entity/Comment.java
@@ -1,6 +1,10 @@
 package ir.maktab.model.entity;
 
+import ir.maktab.model.entity.users.Customer;
+
 public class Comment {
     private double score;
     private String opinion;
+    private Customer customer;
+    private Order order;
 }
