a33a2fcd4d76e612b0a9e15652be22b2ee0d3f66
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 12:17:47 +0330

    add isManagerExist & save method to ManagerService

---
+++ src/main/java/ir/maktab/service/ManagerService.java
@@ -0,0 +1,30 @@
+package ir.maktab.service;
+
+import ir.maktab.dao.ManagerDao;
+import ir.maktab.model.entity.services.MainService;
+import ir.maktab.model.entity.users.Manager;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Optional;
+
+@Getter
+@Setter
+public class ManagerService {
+    private ManagerDao managerDao;
+
+    public void save(Manager manager) {
+        managerDao.save(manager);
+    }
+
+    public Boolean isManagerExist(String username) {
+        Boolean exist = false;
+        Optional<Manager> manager = managerDao.findByUsername(username);
+        if (manager.isPresent()) {
+            exist = true;
+        } else {
+            throw new RuntimeException("username not exist!");
+        }
+        return exist;
+    }
+}
