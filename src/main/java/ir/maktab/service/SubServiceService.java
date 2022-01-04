2e8aced26333cdf64776e7b1e3af702051eb168d
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 14:02:29 +0330

    add save & findByName method to SubServiceService

--- src/main/java/ir/maktab/service/SubServiceService.java
+++ src/main/java/ir/maktab/service/SubServiceService.java
@@ -1,11 +1,29 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.SubServiceDao;
+import ir.maktab.exception.NotExistException;
+import ir.maktab.model.entity.services.MainService;
+import ir.maktab.model.entity.services.SubService;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Optional;
 
 @Getter
 @Setter
 public class SubServiceService {
     private SubServiceDao subServiceDao;
+
+    public void save(SubService subService) {
+        subServiceDao.save(subService);
+    }
+
+    public SubService isSubServiceExist(String name) {
+        Optional<SubService> subService = subServiceDao.findByName(name);
+        if (subService.isPresent()) {
+            return subService.get();
+        } else {
+            throw new NotExistException("this subService not exist!");
+        }
+    }
 }
