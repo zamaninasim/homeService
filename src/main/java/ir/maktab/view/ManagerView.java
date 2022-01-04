ddf5ebe722348489f8ab6a97d4acf53fc6df2383
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 13:08:46 +0330

    add login manager method to ManagerView

--- src/main/java/ir/maktab/view/ManagerView.java
+++ src/main/java/ir/maktab/view/ManagerView.java
@@ -1,4 +1,28 @@
 package ir.maktab.view;
 
+import ir.maktab.exception.InvalidPasswordException;
+import ir.maktab.model.entity.users.Manager;
+import ir.maktab.service.ManagerService;
+
+import java.util.Scanner;
+
 public class ManagerView {
+    final static Scanner scanner = new Scanner(System.in);
+
+    public void managerLogin(ManagerService managerService) {
+        try {
+            System.out.println("enter your username");
+            String username = scanner.nextLine();
+            Manager manager = managerService.findByUsername(username);
+            System.out.println("enter your password");
+            String password = scanner.nextLine();
+            if (password.equals(manager.getPassword())) {
+                //TODO
+            } else {
+                throw new InvalidPasswordException("wrong password!");
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
