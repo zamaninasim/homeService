00be60460ef95f370216ecdbaa98fd66e5fda5b2
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 15:15:28 +0330

    add CustomerBuilder

---
+++ src/main/java/ir/maktab/model/entity/users/CustomerBuilder.java
@@ -0,0 +1,76 @@
+package ir.maktab.model.entity.users;
+
+import ir.maktab.model.enumeration.UserStatus;
+
+import java.util.Date;
+
+public final class CustomerBuilder {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+    private String password;
+    private UserStatus userStatus;
+    private Date registrationDate;
+    private Long credit;
+
+    private CustomerBuilder() {
+    }
+
+    public static CustomerBuilder aCustomer() {
+        return new CustomerBuilder();
+    }
+
+    public CustomerBuilder withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public CustomerBuilder withFirstname(String firstname) {
+        this.firstname = firstname;
+        return this;
+    }
+
+    public CustomerBuilder withLastname(String lastname) {
+        this.lastname = lastname;
+        return this;
+    }
+
+    public CustomerBuilder withEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+        return this;
+    }
+
+    public CustomerBuilder withPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public CustomerBuilder withUserStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+        return this;
+    }
+
+    public CustomerBuilder withRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+        return this;
+    }
+
+    public CustomerBuilder withCredit(Long credit) {
+        this.credit = credit;
+        return this;
+    }
+
+    public Customer build() {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstname(firstname);
+        customer.setLastname(lastname);
+        customer.setEmailAddress(emailAddress);
+        customer.setPassword(password);
+        customer.setUserStatus(userStatus);
+        customer.setRegistrationDate(registrationDate);
+        customer.setCredit(credit);
+        return customer;
+    }
+}
