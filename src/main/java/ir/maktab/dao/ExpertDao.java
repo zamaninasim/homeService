95f3b830368582a14b16fadc5c93eeb9a68df54e
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 15:41:03 +0330

    add findByEmailAddress & save method to ExpertDao

--- src/main/java/ir/maktab/dao/ExpertDao.java
+++ src/main/java/ir/maktab/dao/ExpertDao.java
@@ -1,4 +1,34 @@
 package ir.maktab.dao;
 
+import ir.maktab.model.entity.users.Customer;
+import ir.maktab.model.entity.users.Expert;
+import ir.maktab.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
+
 public class ExpertDao {
+    private SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+
+    public void save(Expert expert) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(expert);
+        transaction.commit();
+        session.close();
+    }
+
+    public Optional<Expert> findByEmailAddress(String email) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Expert> query = session.createQuery("FROM Expert expert WHERE expert.emailAddress=:emailValue");
+        query.setParameter("emailValue", email);
+        Optional<Expert> expert = Optional.ofNullable(query.uniqueResult());
+        transaction.commit();
+        session.close();
+        return expert;
+    }
 }
