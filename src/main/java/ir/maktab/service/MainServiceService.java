4b90a43994b3753f781e4f064f38af97d9987327
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 11:56:02 +0330

    add isMainServiceExist method to MainServiceService

--- src/main/java/ir/maktab/service/MainServiceService.java
+++ src/main/java/ir/maktab/service/MainServiceService.java
@@ -5,6 +5,8 @@ import ir.maktab.model.entity.services.MainService;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Optional;
+
 @Getter
 @Setter
 public class MainServiceService {
@@ -12,5 +14,16 @@ public class MainServiceService {
 
     public void save(MainService mainService) {
         mainServiceDao.save(mainService);
+    }
+
+    public Boolean isMainServiceExist(String name) {
+        Boolean exist = false;
+        Optional<MainService> mainService = mainServiceDao.findByName(name);
+        if (mainService.isPresent()) {
+            exist=true;
+        } else {
+            throw new RuntimeException("this mainService not exist!");
+        }
+        return exist;
     }
 }
