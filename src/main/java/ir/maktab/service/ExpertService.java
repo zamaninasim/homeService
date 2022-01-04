875bbe7154ec44c2a2ad45183b09dd5b9d7c9029
nasim zamani <zamaninasim213@gmail.com>
2022-01-04 15:43:52 +0330

    modify ExpertService

--- src/main/java/ir/maktab/service/ExpertService.java
+++ src/main/java/ir/maktab/service/ExpertService.java
@@ -1,11 +1,38 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.ExpertDao;
+import ir.maktab.exception.ExistException;
+import ir.maktab.model.entity.users.Customer;
+import ir.maktab.model.entity.users.Expert;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Optional;
 
 @Getter
 @Setter
 public class ExpertService {
     private ExpertDao expertDao;
+    public void save(Expert expert) {
+        expertDao.save(expert);
+    }
+
+    public Expert findByEmailAddress(String emailAddress) {
+        Optional<Expert> expert = expertDao.findByEmailAddress(emailAddress);
+        if (expert.isPresent()) {
+            Expert foundedExpert = expert.get();
+            return foundedExpert;
+        } else {
+            throw new RuntimeException("emailAddress not exist!");
+        }
+    }
+
+    public boolean isExist(String emailAddress) {
+        Optional<Expert> expert = expertDao.findByEmailAddress(emailAddress);
+        if (expert.isPresent()) {
+            throw new ExistException("this emailAddress exist!");
+        } else {
+            return false;
+        }
+    }
 }
