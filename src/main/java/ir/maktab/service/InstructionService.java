5d85158ec12273f4655d31a6834daef204b01e6b
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 01:16:08 +0330

    add findByCustomer method to InstructionService

--- src/main/java/ir/maktab/service/InstructionService.java
+++ src/main/java/ir/maktab/service/InstructionService.java
@@ -2,10 +2,13 @@ package ir.maktab.service;
 
 import ir.maktab.dao.InstructionDao;
 import ir.maktab.model.entity.Instruction;
+import ir.maktab.model.entity.users.Customer;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,5 +23,9 @@ public class InstructionService {
 
     public Instruction get(Integer id) {
         return instructionDao.get(id);
+    }
+
+    public List<Instruction> findByCustomer(Customer customer) {
+        return instructionDao.findByCustomer(customer);
     }
 }
