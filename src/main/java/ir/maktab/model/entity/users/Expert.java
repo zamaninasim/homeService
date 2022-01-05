b99d4fb2dd2421df41911062d924ff4df500d00d
nasim zamani <zamaninasim213@gmail.com>
2022-01-05 16:12:10 +0330

    modify toString Expert

--- src/main/java/ir/maktab/model/entity/users/Expert.java
+++ src/main/java/ir/maktab/model/entity/users/Expert.java
@@ -3,18 +3,26 @@ package ir.maktab.model.entity.users;
 import ir.maktab.model.entity.services.SubService;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+
 @Data
 @Entity
 public class Expert extends User {
     @Lob
-    @Column(columnDefinition="BLOB")
+    @Column(columnDefinition = "BLOB")
     private byte[] photo;
     private Double score;
-    @ManyToMany
-    private Set<SubService> services;
+    @ManyToMany(mappedBy = "experts",fetch = FetchType.EAGER)
+    private Set<SubService> services = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return "Expert{" +
+                "photo=" + Arrays.toString(photo) +
+                ", score=" + score +
+                '}';
+    }
 }
