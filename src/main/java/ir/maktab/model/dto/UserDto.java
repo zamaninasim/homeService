031196566a05bfe923a9cc7b91d77e8eb0b6bca4
nasim zamani <zamaninasim213@gmail.com>
2022-01-05 21:26:49 +0330

    add UserDto

---
+++ src/main/java/ir/maktab/model/dto/UserDto.java
@@ -0,0 +1,17 @@
+package ir.maktab.model.dto;
+
+import ir.maktab.model.enumeration.Role;
+import ir.maktab.model.enumeration.UserStatus;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class UserDto {
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+    private UserStatus userStatus;
+    private Date registrationDate;
+    private Long credit;
+    private Role role;
+}
