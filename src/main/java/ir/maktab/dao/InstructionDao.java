7446eb198ac091ba3176bf7fa67ee6d28894d31b
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 01:10:52 +0330

    add findByCustomer method to InstructionDao

--- src/main/java/ir/maktab/dao/InstructionDao.java
+++ src/main/java/ir/maktab/dao/InstructionDao.java
@@ -1,11 +1,15 @@
 package ir.maktab.dao;
 
 import ir.maktab.model.entity.Instruction;
+import ir.maktab.model.entity.users.Customer;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -27,5 +31,16 @@ public class InstructionDao {
         transaction.commit();
         session.close();
         return instruction;
+    }
+
+    public List<Instruction> findByCustomer(Customer customer) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Instruction> query = session.createQuery("FROM Instruction i WHERE i.customer=:customerValue");
+        query.setParameter("customerValue", customer);
+        List<Instruction> instructions = query.list();
+        transaction.commit();
+        session.close();
+        return instructions;
     }
 }
