f2d193b1c874830b5be236a993cf20ae30336462
nasim zamani <zamaninasim213@gmail.com>
2022-01-05 19:20:57 +0330

    add findUserByCondition method UserService

--- src/main/java/ir/maktab/service/UserService.java
+++ src/main/java/ir/maktab/service/UserService.java
@@ -2,9 +2,11 @@ package ir.maktab.service;
 
 import ir.maktab.dao.UserDao;
 import ir.maktab.model.entity.users.User;
+import ir.maktab.model.enumeration.Role;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.Optional;
 
 @Getter
@@ -28,5 +30,9 @@ public class UserService {
         } else {
             throw new RuntimeException("emailAddress not exist!");
         }
+    }
+
+    public List<User> findUserByCondition(String firstname, String lastname, String email, Role role) {
+        return userDao.findUserByCondition(firstname,lastname,email,role);
     }
 }
