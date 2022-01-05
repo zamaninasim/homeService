65d3eed0ec9e343636786b57be640245513d9cfd
nasim zamani <zamaninasim213@gmail.com>
2022-01-05 12:49:58 +0330

    add ImageReader.java

---
+++ src/main/java/ir/maktab/model/ImageReader.java
@@ -0,0 +1,17 @@
+package ir.maktab.model;
+
+import java.io.*;
+
+public class ImageReader {
+    public byte[] fileToBytes(String filename) throws IOException {
+        final byte[] buffer = new byte[256];
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            try (InputStream in = new FileInputStream(new File(filename))) {
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) > 0)
+                    out.write(buffer, 0, bytesRead);
+            }
+            return out.toByteArray();
+        }
+    }
+}
