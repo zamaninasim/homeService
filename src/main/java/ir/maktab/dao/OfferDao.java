d35e75b9f1defa2419abc56b6b1cf85a8d726dcc
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 00:08:44 +0330

    add save method to OfferDao

--- src/main/java/ir/maktab/dao/OfferDao.java
+++ src/main/java/ir/maktab/dao/OfferDao.java
@@ -1,11 +1,22 @@
 package ir.maktab.dao;
 
+import ir.maktab.model.entity.Offer;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class OfferDao {
     private final SessionFactory sessionFactory;
+
+    public void save(Offer offer) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(offer);
+        transaction.commit();
+        session.close();
+    }
 }
