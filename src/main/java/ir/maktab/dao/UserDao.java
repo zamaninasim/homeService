2d6e9909119baa236d0e629efe572a1bbee2ed0a
nasim zamani <zamaninasim213@gmail.com>
2022-01-05 19:19:34 +0330

    add findUserByCondition method to UserDao

--- src/main/java/ir/maktab/dao/UserDao.java
+++ src/main/java/ir/maktab/dao/UserDao.java
@@ -1,12 +1,18 @@
 package ir.maktab.dao;
 
 import ir.maktab.model.entity.users.User;
+import ir.maktab.model.enumeration.Role;
 import ir.maktab.util.HibernateUtil;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
 import org.hibernate.query.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public class UserDao {
@@ -37,5 +43,27 @@ public class UserDao {
         transaction.commit();
         session.close();
         return user;
+    }
+
+    public List<User> findUserByCondition(String firstname, String lastname, String email, Role role) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Criteria criteria = session.createCriteria(User.class, "u");
+        if (firstname != null) {
+            criteria.add(Restrictions.eq("u.firstname", firstname));
+        }
+        if (lastname != null) {
+            criteria.add(Restrictions.eq("u.lastname", lastname));
+        }
+        if (email != null) {
+            criteria.add(Restrictions.eq("u.emailAddress", email));
+        }
+        if (role != null) {
+            criteria.add(Restrictions.eq("u.role", role));
+        }
+        List<User> users = criteria.list();
+        transaction.commit();
+        session.close();
+        return users;
     }
 }
