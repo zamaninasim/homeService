c7ddd58251430c03192cc8ac9814935eb8cf3d7d
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 23:20:12 +0330

    add addOfferToInstruction method in OfferService

--- src/main/java/ir/maktab/service/OfferService.java
+++ src/main/java/ir/maktab/service/OfferService.java
@@ -2,10 +2,14 @@ package ir.maktab.service;
 
 import ir.maktab.dao.OfferDao;
 import ir.maktab.model.entity.Offer;
+import ir.maktab.model.entity.services.SubService;
+import ir.maktab.model.enumeration.InstructionStatus;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,5 +20,16 @@ public class OfferService {
 
     public void save(Offer offer) {
         offerDao.save(offer);
+    }
+
+    public void addOfferToInstruction(Offer offer) {
+        Set<SubService> expertServices = offer.getExpert().getServices();
+        SubService subService = offer.getInstruction().getSubService();
+        if (expertServices.contains(subService)) {
+            save(offer);
+            offer.getInstruction().setOrderStatus(InstructionStatus.WAITING_FOR_EXPERT_SELECTION);
+        } else {
+            throw new RuntimeException("this Instruction service is not in your field.");
+        }
     }
 }
