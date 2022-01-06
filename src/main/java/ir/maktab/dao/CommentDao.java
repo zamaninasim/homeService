478796e68cbc45ecb26f3932f9a0f7f9e084b466
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 20:35:42 +0330

    add save method to CommentDao

--- src/main/java/ir/maktab/dao/CommentDao.java
+++ src/main/java/ir/maktab/dao/CommentDao.java
@@ -1,11 +1,22 @@
 package ir.maktab.dao;
 
+import ir.maktab.model.entity.Comment;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class CommentDao {
     private final SessionFactory sessionFactory;
+
+    public void save(Comment comment) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(comment);
+        transaction.commit();
+        session.close();
+    }
 }
