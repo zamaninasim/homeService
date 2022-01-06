bc3b11f62fd73bae622dea01ef417928c55f720f
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 23:16:10 +0330

    add equal&hash to Offer

--- src/main/java/ir/maktab/model/entity/Offer.java
+++ src/main/java/ir/maktab/model/entity/Offer.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -37,5 +38,18 @@ public class Offer {
                 ", startTime=" + startTime +
                 ", offerStatus=" + offerStatus +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offer offer = (Offer) o;
+        return durationOfWork == offer.durationOfWork && Objects.equals(id, offer.id) && Objects.equals(expert, offer.expert) && Objects.equals(instruction, offer.instruction) && Objects.equals(registrationDate, offer.registrationDate) && Objects.equals(proposedPrice, offer.proposedPrice) && Objects.equals(startTime, offer.startTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, expert, instruction, registrationDate, proposedPrice, durationOfWork, startTime);
     }
 }
