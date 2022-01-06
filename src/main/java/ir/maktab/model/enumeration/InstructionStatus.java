ae0313482bbdf9ffa6e6cdcfe90ef376c5f861bb
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 23:16:48 +0330

    modify InstructionStatus enum

---
+++ src/main/java/ir/maktab/model/enumeration/InstructionStatus.java
@@ -0,0 +1,10 @@
+package ir.maktab.model.enumeration;
+
+public enum InstructionStatus {
+    WAITING_FOR_EXPERT_SUGGESTIONS,
+    WAITING_FOR_EXPERT_SELECTION,
+    WAITING_FOR_THE_EXPERT_TO_ARRIVE,
+    STARTED,
+    DONE,
+    PAID;
+}
