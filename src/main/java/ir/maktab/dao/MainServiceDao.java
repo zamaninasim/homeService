9a99e5a49ab64daeca6399062f11374c533d792f
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 23:34:11 +0330

    add CRUD method To MainServiceDao

--- src/main/java/ir/maktab/dao/MainServiceDao.java
+++ src/main/java/ir/maktab/dao/MainServiceDao.java
@@ -9,6 +9,7 @@ import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MainServiceDao {
@@ -21,12 +22,38 @@ public class MainServiceDao {
         transaction.commit();
         session.close();
     }
+
     public Optional<MainService> findByName(String name) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         Query<MainService> query = session.createQuery("FROM MainService mainService WHERE mainService.name=:nameValue");
         query.setParameter("nameValue", name);
         Optional<MainService> mainService = Optional.ofNullable(query.uniqueResult());
+        transaction.commit();
+        session.close();
+        return mainService;
+    }
+
+    public void delete(MainService mainService) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(mainService);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(MainService mainService) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(mainService);
+        transaction.commit();
+        session.close();
+    }
+
+    public MainService findById(Integer id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        MainService mainService = session.get(MainService.class, id);
         transaction.commit();
         session.close();
         return mainService;
