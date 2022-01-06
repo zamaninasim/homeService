2e49bcf09a69859b770f1d01a01e7348836b9da8
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 20:38:44 +0330

    modify Exceptions class

--- src/main/java/ir/maktab/validation/exception/ExistException.java
+++
@@ -1,7 +0,0 @@
-package ir.maktab.validation.exception;
-
-public class ExistException extends RuntimeException{
-    public ExistException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/ir/maktab/validation/exception/IsExistException.java
@@ -0,0 +1,7 @@
+package ir.maktab.validation.exception;
+
+public class IsExistException extends RuntimeException{
+    public IsExistException(String message) {
+        super(message);
+    }
+}
