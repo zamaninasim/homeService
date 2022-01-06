1a9692a8fde3709d6e6ff355d4f9f5fafc421ca3
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 20:42:37 +0330

    add save method to CommentService

--- src/main/java/ir/maktab/service/CommentService.java
+++ src/main/java/ir/maktab/service/CommentService.java
@@ -1,6 +1,7 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.CommentDao;
+import ir.maktab.model.entity.Comment;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -12,4 +13,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentDao commentDao;
+
+    public void save(Comment comment) {
+        commentDao.save(comment);
+    }
 }
