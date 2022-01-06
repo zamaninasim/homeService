efe66179f29e24d3b7a68108d66288d5a0d92902
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 20:43:52 +0330

    modify SubService methods

--- src/main/java/ir/maktab/service/SubServiceService.java
+++ src/main/java/ir/maktab/service/SubServiceService.java
@@ -2,7 +2,9 @@ package ir.maktab.service;
 
 import ir.maktab.dao.SubServiceDao;
 import ir.maktab.model.entity.services.SubService;
+import ir.maktab.model.entity.users.Expert;
 import ir.maktab.validation.exception.IsExistException;
+import ir.maktab.validation.exception.NotExistException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -42,11 +44,19 @@ public class SubServiceService {
 
     public SubService findByName(String name) {
         List<SubService> subServices = subServiceDao.findByName(name);
+        if (subServices.isEmpty()) {
+            throw new NotExistException("this subService not exist!");
+        }
         SubService subService = subServices.get(0);
         return subService;
     }
 
     public List<SubService> findAll() {
         return subServiceDao.findAll();
+    }
+
+    public void addExpertToSubService(Expert expert, SubService subService) {
+        subService.getExperts().add(expert);
+        update(subService);
     }
 }
