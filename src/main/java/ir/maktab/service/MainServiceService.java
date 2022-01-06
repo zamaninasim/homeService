6a4a7c12ba52df0ccac85fc6f1dcc8122d1484d1
nasim zamani <zamaninasim213@gmail.com>
2022-01-06 18:37:45 +0330

    modify save method

--- src/main/java/ir/maktab/service/MainServiceService.java
+++ src/main/java/ir/maktab/service/MainServiceService.java
@@ -1,6 +1,7 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.MainServiceDao;
+import ir.maktab.validation.exception.IsExistException;
 import ir.maktab.validation.exception.NotExistException;
 import ir.maktab.model.entity.services.MainService;
 import lombok.Getter;
@@ -18,10 +19,17 @@ public class MainServiceService {
     private final MainServiceDao mainServiceDao;
 
     public void save(MainService mainService) {
-        mainServiceDao.save(mainService);
+        Optional<MainService> foundedMainService = mainServiceDao.findByName(mainService.getName());
+        if (foundedMainService.isPresent()) {
+            throw new IsExistException("this mainService exist!");
+        } else {
+            mainServiceDao.save(mainService);
+            System.out.println("mainService "+mainService.getName()+" saved.");
+        }
+
     }
 
-    public MainService findMainService(String name) {
+    public MainService findByName(String name) {
         Optional<MainService> mainService = mainServiceDao.findByName(name);
         if (mainService.isPresent()) {
             return mainService.get();
