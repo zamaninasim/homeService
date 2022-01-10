a5be00eef6d00980b05a602e1b65319ca57e3a29
nasim zamani <zamaninasim213@gmail.com>
2022-01-10 10:59:51 +0330

    modify ManagerDao.java to ManagerRepository.java

---
+++ src/main/java/ir/maktab/data/dao/ManagerRepository.java
@@ -0,0 +1,12 @@
+package ir.maktab.data.dao;
+
+import ir.maktab.data.model.entity.users.Manager;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ManagerRepository extends CrudRepository<Manager, Integer> {
+    Optional<Manager> findByUsername(String username);
+}
