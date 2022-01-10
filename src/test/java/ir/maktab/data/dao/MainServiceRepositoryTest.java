aa594a51ccc02e1e12421855a16025caa4ca91d9
nasim zamani <zamaninasim213@gmail.com>
2022-01-10 11:40:58 +0330

    add MainServiceRepositoryTest.java

---
+++ src/test/java/ir/maktab/data/dao/MainServiceRepositoryTest.java
@@ -0,0 +1,31 @@
+package ir.maktab.data.dao;
+
+import ir.maktab.data.model.entity.services.MainService;
+import ir.maktab.service.MainServiceService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainServiceRepositoryTest {
+    @Mock
+    private MainServiceRepository mainServiceRepository;
+    @InjectMocks
+    private MainServiceService mainServiceService;
+    @Test
+    public void whenSaveMainService_shouldReturnMainService() {
+        MainService mainService = MainService.builder().name("a").build();
+        when(mainServiceRepository.save(ArgumentMatchers.any(MainService.class))).thenReturn(mainService);
+        MainService created = mainServiceService.save(mainService);
+        assertThat(created.getName()).isSameAs(mainService.getName());
+        verify(mainServiceRepository).save(mainService);
+    }
+}
