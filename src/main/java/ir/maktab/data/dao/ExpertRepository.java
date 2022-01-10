21ebb0e1f5d827809c8a07f7d8dc7ca66e62388b
nasim zamani <zamaninasim213@gmail.com>
2022-01-10 10:54:06 +0330

    modify ExpertDao.java to ExpertRepository.java

---
+++ src/main/java/ir/maktab/data/dao/ExpertRepository.java
@@ -0,0 +1,12 @@
+package ir.maktab.data.dao;
+
+import ir.maktab.data.model.entity.users.Expert;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExpertRepository extends CrudRepository<Expert, Integer> {
+    Optional<Expert> findByEmailAddress(String email);
+}
