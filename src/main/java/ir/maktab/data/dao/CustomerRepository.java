caa590ec0da3d2c0e35d58643e55462ba5d2542d
nasim zamani <zamaninasim213@gmail.com>
2022-01-10 10:53:32 +0330

    modify CustomerDao.java to CustomerService.java

---
+++ src/main/java/ir/maktab/data/dao/CustomerRepository.java
@@ -0,0 +1,12 @@
+package ir.maktab.data.dao;
+
+import ir.maktab.data.model.entity.users.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+   Optional<Customer> findByEmailAddress(String email);
+}
