1fcf16c7cd236486385cf83660cc8769d0b097f5
nasim zamani <zamaninasim213@gmail.com>
2022-01-10 10:45:21 +0330

    modify Config.java

--- src/main/java/ir/maktab/config/Config.java
+++ src/main/java/ir/maktab/config/Config.java
@@ -3,12 +3,9 @@ package ir.maktab.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan("ir.maktab")
-@PropertySource("classpath:database.properties")
-@Import({HibernateConfig.class})
+@ComponentScan(basePackages = "ir.maktab")
+@Import(value = {DatabaseConfig.class})
 public class Config {
-
 }
