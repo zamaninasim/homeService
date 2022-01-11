232af298d1e168c44a4e5b7b646e4a5a0d34df99
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 14:41:53 +0330

    modify OfferService methods

--- src/main/java/ir/maktab/service/OfferService.java
+++ src/main/java/ir/maktab/service/OfferService.java
@@ -1,9 +1,12 @@
 package ir.maktab.service;
 
 import ir.maktab.data.dao.OfferRepository;
+import ir.maktab.data.dao.OrderRepository;
 import ir.maktab.data.model.entity.Offer;
+import ir.maktab.data.model.entity.Order;
 import ir.maktab.data.model.entity.services.SubService;
 import ir.maktab.data.model.enumeration.OrderStatus;
+import ir.maktab.exception.NotMatchException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,19 +16,22 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class OfferService {
     private final OfferRepository offerRepository;
+    private final OrderRepository orderRepository;
 
     public void save(Offer offer) {
         offerRepository.save(offer);
     }
 
-    public void addOfferToInstruction(Offer offer) {
+    public void addOfferToOrder(Offer offer) {
         Set<SubService> expertServices = offer.getExpert().getServices();
         SubService subService = offer.getOrder().getSubService();
         if (expertServices.contains(subService)) {
             save(offer);
-            offer.getOrder().setOrderStatus(OrderStatus.WAITING_FOR_EXPERT_SELECTION);
+            Order order = offer.getOrder();
+            order.setOrderStatus(OrderStatus.WAITING_FOR_EXPERT_SELECTION);
+            orderRepository.save(order);
         } else {
-            throw new RuntimeException("this Instruction service is not in your field.");
+            throw new NotMatchException("this Instruction service is not in your field.");
         }
     }
 }
