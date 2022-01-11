b4294212e3b071b060290c5f46ee32479a453844
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 12:54:08 +0330

    add SubServiceServiceTest.java

--- src/test/java/ir/maktab/service/SubServiceServiceTest.java
+++ src/test/java/ir/maktab/service/SubServiceServiceTest.java
@@ -1,4 +1,12 @@
 package ir.maktab.service;
 
+import ir.maktab.config.Config;
+import ir.maktab.data.model.entity.services.SubService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SubServiceServiceTest {
+    ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+    SubServiceService subServiceService = context.getBean(SubServiceService.class);
+    SubService subService;
 }
