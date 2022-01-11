3bf950a8d99c4fa6245a84b2340a668f8f07c5d3
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 12:02:42 +0330

    modify regex in Validation.java

--- src/main/java/ir/maktab/validation/Validation.java
+++ src/main/java/ir/maktab/validation/Validation.java
@@ -4,7 +4,9 @@ import ir.maktab.exception.InvalidEmailException;
 import ir.maktab.exception.InvalidNameException;
 import ir.maktab.exception.InvalidNumberException;
 import ir.maktab.exception.InvalidPasswordException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Validation {
     public boolean validateEmail(String email) throws InvalidEmailException {
         if (email.matches("^((\"[\\w-\\s]+\")|([\\w-]+(?:\\.[\\w-]+)*)|(\"[\\w-\\s]+\")([\\w-]+(?:\\.[\\w-]+)*))(@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$)|(@\\[?((25[0-5]\\.|2[0-4][0-9]\\.|1[0-9]{2}\\.|[0-9]{1,2}\\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\\]?$)")) {
@@ -14,22 +16,23 @@ public class Validation {
     }
 
     public boolean validateName(String name) throws InvalidNameException {
-        if (name.length() > 2 && name.matches("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")) {
+        if (name.length() > 2 && name.matches("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$") && name.length() < 20) {
             return true;
         }
         throw new InvalidNameException("invalid name!");
     }
 
     public boolean validateNumber(String number) throws InvalidNumberException {
-        if (number.matches("^[0-9]*$")) {
+        if (number.matches("^([+]?[1-9]\\d*|0)$")) {
             return true;
         }
-        throw new InvalidNameException("invalid number!");
+        throw new InvalidNumberException("invalid number!");
     }
+
     public boolean validatePassword(String password) throws InvalidPasswordException {
         if (password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")) {
             return true;
         }
-        throw new InvalidNameException("invalid password!");
+        throw new InvalidPasswordException("invalid password!");
     }
 }
