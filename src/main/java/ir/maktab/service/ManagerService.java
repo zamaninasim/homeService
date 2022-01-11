2f9ebebe8a7378f888b9c3ff0ed9deec8810eb62
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 14:41:34 +0330

    modify ManagerService methods

--- src/main/java/ir/maktab/service/ManagerService.java
+++ src/main/java/ir/maktab/service/ManagerService.java
@@ -2,6 +2,7 @@ package ir.maktab.service;
 
 import ir.maktab.data.dao.ManagerRepository;
 import ir.maktab.data.model.entity.users.Manager;
+import ir.maktab.exception.EntityNotExistException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,24 +17,8 @@ public class ManagerService {
         managerRepository.save(manager);
     }
 
-    public Boolean isManagerExist(String username) {
-        Boolean exist = false;
-        Optional<Manager> manager = managerRepository.findByUsername(username);
-        if (manager.isPresent()) {
-            exist = true;
-        } else {
-            throw new RuntimeException("username not exist!");
-        }
-        return exist;
-    }
-
     public Manager findByUsername(String username) {
         Optional<Manager> manager = managerRepository.findByUsername(username);
-        if (manager.isPresent()) {
-            Manager foundedManager = manager.get();
-            return foundedManager;
-        } else {
-            throw new RuntimeException("username not exist!");
-        }
+        return manager.orElseThrow(() -> new EntityNotExistException("username not exist!"));
     }
 }
