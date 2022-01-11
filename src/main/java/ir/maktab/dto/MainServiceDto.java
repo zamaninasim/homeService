1c2982763c061461fde36122b14bc746cbdb44c4
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 12:00:17 +0330

    add MainServiceDto.java

---
+++ src/main/java/ir/maktab/dto/MainServiceDto.java
@@ -0,0 +1,14 @@
+package ir.maktab.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainServiceDto {
+    private String name;
+}
