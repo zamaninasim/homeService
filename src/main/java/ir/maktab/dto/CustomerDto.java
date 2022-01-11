be821e983a73bc515bd8f8fa8dc65547c925de0e
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 11:59:41 +0330

    add CustomerDto.java

---
+++ src/main/java/ir/maktab/dto/CustomerDto.java
@@ -0,0 +1,26 @@
+package ir.maktab.dto;
+
+import ir.maktab.data.model.enumeration.Role;
+import ir.maktab.data.model.enumeration.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDto {
+    private Long identificationNumber;
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+    private UserStatus userStatus;
+    private Date registrationDate;
+    private Long credit;
+    private Role role;
+}
