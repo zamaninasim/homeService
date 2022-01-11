541d08a0f49883e389bc1b3d21bc6d99f3c6fa61
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 11:46:55 +0330

    clean OrderRepository.java

--- src/main/java/ir/maktab/data/dao/OrderRepository.java
+++ src/main/java/ir/maktab/data/dao/OrderRepository.java
@@ -4,6 +4,8 @@ import ir.maktab.data.model.entity.Order;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Integer> {
 }
