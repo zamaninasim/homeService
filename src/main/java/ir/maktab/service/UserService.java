f10875e0d6ce0328da72b563cd881b7a2b9f234e
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 14:42:57 +0330

    modify UserService methods

--- src/main/java/ir/maktab/service/UserService.java
+++ src/main/java/ir/maktab/service/UserService.java
@@ -1,54 +1,52 @@
 package ir.maktab.service;
 
-import ir.maktab.dao.UserDao;
-import ir.maktab.model.dto.UserDto;
-import ir.maktab.model.entity.users.User;
-import ir.maktab.model.enumeration.Role;
-import ir.maktab.service.mapper.Mapper;
-import ir.maktab.validation.exception.IsExistException;
-import lombok.Getter;
+import ir.maktab.data.dao.UserRepository;
+import ir.maktab.data.model.entity.users.User;
+import ir.maktab.data.model.enumeration.Role;
+import ir.maktab.dto.UserDto;
+import ir.maktab.dto.mapper.OfferMapper;
+import ir.maktab.dto.mapper.UserMapper;
+import ir.maktab.exception.EntityIsExistException;
+import ir.maktab.exception.EntityNotExistException;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Getter
-@Setter
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserDao userDao;
-    Mapper mapper = new Mapper();
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public void save(User user) {
-        Optional<User> foundedUser = userDao.findByEmailAddress(user.getEmailAddress());
+        Optional<User> foundedUser = userRepository.findByEmailAddress(user.getEmailAddress());
         if (foundedUser.isPresent()) {
-            throw new IsExistException("this emailAddress exist!");
+            throw new EntityIsExistException("this emailAddress exist!");
         } else {
-            userDao.save(user);
-            System.out.println("user whit '" + user.getEmailAddress() + "' emailAddress saved.");
+            userRepository.save(user);
         }
     }
 
     public void update(User user) {
-        userDao.update(user);
+        userRepository.save(user);
     }
 
     public User findByEmailAddress(String emailAddress) {
-        Optional<User> expert = userDao.findByEmailAddress(emailAddress);
-        if (expert.isPresent()) {
-            return expert.get();
+        Optional<User> user = userRepository.findByEmailAddress(emailAddress);
+        if (user.isPresent()) {
+            return user.get();
         } else {
-            throw new RuntimeException("emailAddress not exist!");
+            throw new EntityNotExistException("emailAddress not exist!");
         }
     }
 
     public List<UserDto> findUserByCondition(String firstname, String lastname, String email, Role role) {
-        List<User> users = userDao.findUserByCondition(firstname, lastname, email, role);
-        List<UserDto> userDtos = users.stream().map(mapper::userDto).collect(Collectors.toList());
+        List<User> users = userRepository.findUserByCondition(firstname, lastname, email, role);
+        List<UserDto> userDtos = users.stream().map(userMapper::userToUserDto).collect(Collectors.toList());
         return userDtos;
     }
 
