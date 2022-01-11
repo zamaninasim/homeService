d79243ba8821ce22d9c990bc96b0d4e9982b2cc5
nasim zamani <zamaninasim213@gmail.com>
2022-01-11 14:42:09 +0330

    modify OrderService methods

--- src/main/java/ir/maktab/service/OrderService.java
+++ src/main/java/ir/maktab/service/OrderService.java
@@ -5,6 +5,7 @@ import ir.maktab.data.model.entity.Offer;
 import ir.maktab.data.model.entity.Order;
 import ir.maktab.data.model.enumeration.OfferStatus;
 import ir.maktab.data.model.enumeration.OrderStatus;
+import ir.maktab.exception.EntityIsExistException;
 import ir.maktab.exception.EntityNotExistException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,17 +19,17 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     public void save(Order order) {
-        orderRepository.save(order);
+        Optional<Order> orderByTrackingNumber = orderRepository.findById(order.getId());
+        if (orderByTrackingNumber.isPresent()) {
+            throw new EntityIsExistException("this order is exist!");
+        } else {
+            orderRepository.save(order);
+        }
     }
 
     public Order findById(Integer id) {
         Optional<Order> order = orderRepository.findById(id);
-        if (order.isPresent()){
-            Order foundedOrder = order.get();
-            return foundedOrder;
-        }else {
-            throw new EntityNotExistException("this order not exist!");
-        }
+        return order.orElseThrow(() -> new EntityNotExistException("this order not exist!"));
     }
 
     public Offer findAcceptedOfferOfInstruction(Order order) {
