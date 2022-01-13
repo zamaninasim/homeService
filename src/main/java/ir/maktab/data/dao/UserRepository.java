d24920073c3c175e1abda3517336defcf0e68b40
nasim zamani <zamaninasim213@gmail.com>
2022-01-13 18:54:45 +0330

    add selectByCondition method in UserRepository

--- src/main/java/ir/maktab/data/dao/UserRepository.java
+++ src/main/java/ir/maktab/data/dao/UserRepository.java
@@ -1,34 +1,37 @@
 package ir.maktab.data.dao;
 
 import ir.maktab.data.model.entity.users.User;
+import ir.maktab.data.model.enumeration.Role;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>, UserRepositoryCustom {
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
     Optional<User> findByEmailAddress(String email);
 
-    /*public List<User> findUserByCondition(String firstname, String lastname, String email, Role role) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Criteria criteria = session.createCriteria(User.class, "u");
-        if (firstname != null) {
-            criteria.add(Restrictions.eq("u.firstname", firstname));
-        }
-        if (lastname != null) {
-            criteria.add(Restrictions.eq("u.lastname", lastname));
-        }
-        if (email != null) {
-            criteria.add(Restrictions.eq("u.emailAddress", email));
-        }
-        if (role != null) {
-            criteria.add(Restrictions.eq("u.role", role));
-        }
-        List<User> users = criteria.list();
-        transaction.commit();
-        session.close();
-        return users;
-    }*/
+    static Specification<User> selectByCondition(String firstname, String lastname, String email, Role role) {
+        return (root, cq, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (firstname != null) {
+                predicates.add(cb.equal(root.get("firstname"), firstname));
+            }
+            if (lastname != null) {
+                predicates.add(cb.equal(root.get("lastname"), lastname));
+            }
+            if (email != null) {
+                predicates.add(cb.equal(root.get("emailAddress"), email));
+            }
+            if (role != null) {
+                predicates.add(cb.equal(root.get("role"), role));
+            }
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+    }
 }
