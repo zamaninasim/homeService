a268488099793c3d2fdf338440766b3415656300
nasim zamani <zamaninasim213@gmail.com>
2022-01-13 18:53:48 +0330

    add findByOrderAndExpert & findByOrder method to OfferRepository

--- src/main/java/ir/maktab/data/dao/OfferRepository.java
+++ src/main/java/ir/maktab/data/dao/OfferRepository.java
@@ -1,9 +1,18 @@
 package ir.maktab.data.dao;
 
 import ir.maktab.data.model.entity.Offer;
+import ir.maktab.data.model.entity.Order;
+import ir.maktab.data.model.entity.users.Expert;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface OfferRepository extends CrudRepository<Offer, Integer> {
+public interface OfferRepository extends PagingAndSortingRepository<Offer, Integer> {
+    List<Offer> findByOrder(Order order, Sort var1);
+    Optional<Offer> findByOrderAndExpert(Order order, Expert expert);
 }
