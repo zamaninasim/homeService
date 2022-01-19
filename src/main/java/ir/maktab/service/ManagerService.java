3f726b3fc713b20166d8ee89d74966f4d53e9876
nasim zamani <zamaninasim213@gmail.com>
2022-01-19 14:24:10 +0330

    modify ManagerService change entitys to Dto

--- src/main/java/ir/maktab/service/ManagerService.java
+++ src/main/java/ir/maktab/service/ManagerService.java
@@ -2,8 +2,10 @@ package ir.maktab.service;
 
 import ir.maktab.data.dao.ManagerRepository;
 import ir.maktab.data.model.entity.users.Manager;
+import ir.maktab.dto.ManagerDto;
 import ir.maktab.exception.EntityNotExistException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,13 +14,16 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ManagerService {
     private final ManagerRepository managerRepository;
+    private final ModelMapper modelMapper;
 
-    public void save(Manager manager) {
+    public void save(ManagerDto managerDto) {
+        Manager manager = modelMapper.map(managerDto, Manager.class);
         managerRepository.save(manager);
     }
 
-    public Manager findByUsername(String username) {
-        Optional<Manager> manager = managerRepository.findByUsername(username);
-        return manager.orElseThrow(() -> new EntityNotExistException("username not exist!"));
+    public ManagerDto findByUsername(String username) {
+        Optional<Manager> optionalManager = managerRepository.findByUsername(username);
+        Manager manager = optionalManager.orElseThrow(() -> new EntityNotExistException("username not exist!"));
+        return modelMapper.map(manager, ManagerDto.class);
     }
 }
