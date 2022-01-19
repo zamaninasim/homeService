5e095ec6f6f4d50354856fc6be3cc12369950d6a
nasim zamani <zamaninasim213@gmail.com>
2022-01-19 13:43:55 +0330

    modify SubServiceService replace entity to dto

--- src/main/java/ir/maktab/service/SubServiceService.java
+++ src/main/java/ir/maktab/service/SubServiceService.java
@@ -4,10 +4,12 @@ import ir.maktab.data.dao.SubServiceRepository;
 import ir.maktab.data.model.entity.services.SubService;
 import ir.maktab.data.model.entity.users.Expert;
 import ir.maktab.dto.ExpertDto;
+import ir.maktab.dto.SubServiceDto;
 import ir.maktab.dto.mapper.ExpertMapper;
 import ir.maktab.exception.EntityIsExistException;
 import ir.maktab.exception.EntityNotExistException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -23,45 +25,58 @@ public class SubServiceService {
     private final SubServiceRepository subServiceRepository;
     private final MainServiceService mainServiceService;
     private final ExpertMapper expertMapper;
+    private final ModelMapper modelMapper;
 
-    public SubService save(SubService subService) {
+    public void save(SubServiceDto subServiceDto) {
+        SubService subService = modelMapper.map(subServiceDto, SubService.class);
         mainServiceService.findByName(subService.getMainService().getName());
         Optional<SubService> foundedSubService = subServiceRepository.findByName(subService.getName());
         if (foundedSubService.isPresent()) {
             throw new EntityIsExistException("this subService exist!");
         } else {
-            return subServiceRepository.save(subService);
+            subServiceRepository.save(subService);
         }
     }
 
-    public SubService update(SubService subService) {
-        return subServiceRepository.save(subService);
+    public void update(SubServiceDto subServiceDto) {
+        SubService subService = modelMapper.map(subServiceDto, SubService.class);
+        subServiceRepository.save(subService);
     }
 
-    public SubService findByName(String name) {
-        Optional<SubService> subService = subServiceRepository.findByName(name);
-        return subService.orElseThrow(() -> new EntityNotExistException("this subService not exist!"));
+    public SubServiceDto findByName(String name) {
+        Optional<SubService> optionalSubService = subServiceRepository.findByName(name);
+        SubService subService = optionalSubService.orElseThrow(() -> new EntityNotExistException("this subService not exist!"));
+        return modelMapper.map(subService, SubServiceDto.class);
     }
 
-    public List<SubService> findAll() {
-        List<SubService> subServices = new ArrayList<>();
+    public List<SubServiceDto> findAll() {
+        List<SubServiceDto> subServiceDtos = new ArrayList<>();
         Iterable<SubService> subServiceIterable = subServiceRepository.findAll();
-        subServiceIterable.forEach(subServices::add);
-        return subServices;
+        subServiceIterable.forEach(subService -> subServiceDtos.add(modelMapper.map(subService, SubServiceDto.class)));
+        return subServiceDtos;
     }
 
-    public SubService addExpertToSubService(Expert expert, SubService subService) {
+    public SubServiceDto addExpertToSubService(ExpertDto expertDto, SubServiceDto subServiceDto) {
+        Expert expert = modelMapper.map(expertDto, Expert.class);
+        SubService subService = modelMapper.map(subServiceDto, SubService.class);
         subService.getExperts().add(expert);
-        return update(subService);
+        SubServiceDto newSubServiceDto = modelMapper.map(subService, SubServiceDto.class);
+        update(newSubServiceDto);
+        return newSubServiceDto;
     }
 
-    public SubService removeExpertFromSubService(Expert expert, SubService subService) {
+    public SubServiceDto removeExpertFromSubService(ExpertDto expertDto, SubServiceDto subServiceDto) {
+        Expert expert = modelMapper.map(expertDto, Expert.class);
+        SubService subService = modelMapper.map(subServiceDto, SubService.class);
         subService.getExperts().remove(expert);
-        return update(subService);
+        SubServiceDto newSubServiceDto = modelMapper.map(subService, SubServiceDto.class);
+        update(newSubServiceDto);
+        return newSubServiceDto;
     }
 
     public List<ExpertDto> findSubServiceExpertsBySubServiceName(String name) {
-        SubService subService = findByName(name);
+        SubServiceDto subServiceDto = findByName(name);
+        SubService subService = modelMapper.map(subServiceDto, SubService.class);
         Set<Expert> experts = subService.getExperts();
         List<ExpertDto> expertDtos = experts.stream().map(expertMapper::expertToExpertDto).collect(Collectors.toList());
         return expertDtos;
