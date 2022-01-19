417ba34279f24143a7384893b7144a3a8659ef8a
nasim zamani <zamaninasim213@gmail.com>
2022-01-19 15:00:26 +0330

    modify CustomerService change entitys to Dto

--- src/main/java/ir/maktab/service/CustomerService.java
+++ src/main/java/ir/maktab/service/CustomerService.java
@@ -1,15 +1,17 @@
 package ir.maktab.service;
 
 import ir.maktab.data.dao.CustomerRepository;
-import ir.maktab.data.model.entity.Offer;
-import ir.maktab.data.model.entity.Order;
 import ir.maktab.data.model.entity.users.Customer;
-import ir.maktab.data.model.entity.users.Expert;
 import ir.maktab.data.model.enumeration.OfferStatus;
 import ir.maktab.data.model.enumeration.OrderStatus;
+import ir.maktab.dto.CustomerDto;
+import ir.maktab.dto.ExpertDto;
+import ir.maktab.dto.OfferDto;
+import ir.maktab.dto.OrderDto;
 import ir.maktab.exception.EntityIsExistException;
 import ir.maktab.exception.EntityNotExistException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -21,34 +23,37 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final OrderService orderService;
     private final OfferService offerService;
+    private final ModelMapper modelMapper;
 
-    public Customer save(Customer customer) {
+    public void save(CustomerDto customerDto) {
+        Customer customer = modelMapper.map(customerDto, Customer.class);
         Optional<Customer> foundedCustomer = customerRepository.findByEmailAddress(customer.getEmailAddress());
         if (foundedCustomer.isPresent()) {
             throw new EntityIsExistException("this emailAddress exist!");
         } else {
-            return customerRepository.save(customer);
+            customerRepository.save(customer);
         }
     }
 
-    public Customer findByEmailAddress(String emailAddress) {
-        Optional<Customer> customer = customerRepository.findByEmailAddress(emailAddress);
-        return customer.orElseThrow(() -> new EntityNotExistException("emailAddress not exist!"));
+    public CustomerDto findByEmailAddress(String emailAddress) {
+        Optional<Customer> optionalCustomer = customerRepository.findByEmailAddress(emailAddress);
+        Customer customer = optionalCustomer.orElseThrow(() -> new EntityNotExistException("emailAddress not exist!"));
+        return modelMapper.map(customer, CustomerDto.class);
     }
 
-    public void acceptOfferForOrder(Order order, Expert expert) {
-        order.setExpert(expert);
-        order.setOrderStatus(OrderStatus.WAITING_FOR_THE_EXPERT_TO_ARRIVE);
-        orderService.save(order);
-        Offer acceptedOffer = offerService.findByOrderAndExpert(order, expert);
-        Set<Offer> offers = order.getOffers();
-        for (Offer offer : offers) {
-            if (offer.equals(acceptedOffer)) {
-                offer.setOfferStatus(OfferStatus.ACCEPTED);
+    public void acceptOfferForOrder(OrderDto orderDto, ExpertDto expertDto) {
+        orderDto.setExpert(expertDto);
+        orderDto.setOrderStatus(OrderStatus.WAITING_FOR_THE_EXPERT_TO_ARRIVE);
+        orderService.save(orderDto);
+        OfferDto acceptedOfferDto = offerService.findByOrderAndExpert(orderDto, expertDto);
+        Set<OfferDto> offerDtos = orderDto.getOffers();
+        for (OfferDto offerDto : offerDtos) {
+            if (offerDto.equals(acceptedOfferDto)) {
+                offerDto.setOfferStatus(OfferStatus.ACCEPTED);
             } else {
-                offer.setOfferStatus(OfferStatus.REJECTED);
+                offerDto.setOfferStatus(OfferStatus.REJECTED);
             }
-            offerService.save(offer);
+            offerService.save(offerDto);
         }
     }
 }
