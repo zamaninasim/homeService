d8337d4e9c62b2d12d416ca37683112bd5917606
nasim zamani <zamaninasim213@gmail.com>
2022-01-19 15:09:41 +0330

    modify AddressService change entitys to Dto

--- src/main/java/ir/maktab/service/AddressService.java
+++ src/main/java/ir/maktab/service/AddressService.java
@@ -2,8 +2,10 @@ package ir.maktab.service;
 
 import ir.maktab.data.dao.AddressRepository;
 import ir.maktab.data.model.entity.Address;
+import ir.maktab.dto.AddressDto;
 import ir.maktab.exception.EntityNotExistException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,14 +14,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AddressService {
     private final AddressRepository addressRepository;
+    private final ModelMapper modelMapper;
 
-    public Address save(Address address) {
+    public AddressDto save(AddressDto addressDto) {
+        Address address = modelMapper.map(addressDto, Address.class);
         Optional<Address> foundedAddress = addressRepository.findByZipCode(address.getZipCode());
-        return foundedAddress.orElseGet(() -> addressRepository.save(address));
+        Address savedAddress = foundedAddress.orElseGet(() -> addressRepository.save(address));
+        return modelMapper.map(savedAddress, AddressDto.class);
     }
 
-    public Address findByZipCode(Long zipCode) {
-        Optional<Address> address = addressRepository.findByZipCode(zipCode);
-        return address.orElseThrow(() -> new EntityNotExistException("address not exist!"));
+    public AddressDto findByZipCode(Long zipCode) {
+        Optional<Address> optionalAddress = addressRepository.findByZipCode(zipCode);
+        Address address = optionalAddress.orElseThrow(() -> new EntityNotExistException("address not exist!"));
+        return modelMapper.map(address, AddressDto.class);
     }
 }
