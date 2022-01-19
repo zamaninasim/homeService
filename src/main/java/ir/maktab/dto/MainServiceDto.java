15fcfbc8cbef1d5c461c0e98ae81053801ee9e70
nasim zamani <zamaninasim213@gmail.com>
2022-01-19 16:21:16 +0330

    modify MainServiceDto.java

--- src/main/java/ir/maktab/dto/MainServiceDto.java
+++ src/main/java/ir/maktab/dto/MainServiceDto.java
@@ -5,10 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MainServiceDto {
     private String name;
+    private Set<SubServiceDto> subServices = new HashSet<>();
 }
