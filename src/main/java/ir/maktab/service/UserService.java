ea4669fc364f941b63d58489a13aaee098fac690
nasim zamani <zamaninasim213@gmail.com>
2022-01-19 13:25:25 +0330

    modify UserService.java replace user to userdto

--- src/main/java/ir/maktab/service/UserService.java
+++ src/main/java/ir/maktab/service/UserService.java
@@ -3,14 +3,16 @@ package ir.maktab.service;
 import ir.maktab.data.dao.UserRepository;
 import ir.maktab.data.model.entity.users.User;
 import ir.maktab.data.model.enumeration.Role;
-import ir.maktab.dto.mapper.UserMapper;
+import ir.maktab.dto.UserDto;
 import ir.maktab.exception.EntityIsExistException;
 import ir.maktab.exception.EntityNotExistException;
 import ir.maktab.exception.InCorrectException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,41 +21,49 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final ModelMapper modelMapper;
 
-    public User save(User user) {
+    public void save(UserDto userDto) {
+        User user = modelMapper.map(userDto, User.class);
         Optional<User> foundedUser = userRepository.findByEmailAddress(user.getEmailAddress());
         if (foundedUser.isPresent()) {
             throw new EntityIsExistException("this emailAddress exist!");
         } else {
-            return userRepository.save(user);
+            userRepository.save(user);
         }
     }
 
-    public User update(User user) {
-        return userRepository.save(user);
+    public void update(UserDto userDto) {
+        User user = modelMapper.map(userDto, User.class);
+        userRepository.save(user);
     }
 
-    public User findByEmailAddress(String emailAddress) {
-        Optional<User> user = userRepository.findByEmailAddress(emailAddress);
-        if (user.isPresent()) {
-            return user.get();
+    public UserDto findByEmailAddress(String emailAddress) {
+        Optional<User> foundedUser = userRepository.findByEmailAddress(emailAddress);
+        if (foundedUser.isPresent()) {
+            User user = foundedUser.get();
+            return modelMapper.map(user, UserDto.class);
         } else {
             throw new EntityNotExistException("emailAddress not exist!");
         }
     }
 
-    public List<User> findUserByCondition(String firstname, String lastname, String email, Role role) {
+    public List<UserDto> findUserByCondition(String firstname, String lastname, String email, Role role) {
         Specification<User> specification = UserRepository.selectByCondition(firstname, lastname, email, role);
-        return userRepository.findAll(specification);
+        List<User> users = userRepository.findAll(specification);
+        List<UserDto> userDtos = new ArrayList<>();
+        users.stream().forEach(user -> userDtos.add(modelMapper.map(user, UserDto.class)));
+        return userDtos;
     }
 
-    public User changePassword(User user, String currentPassword, String newPassword) {
+    public void changePassword(UserDto userDto, String currentPassword, String newPassword) {
+        User user = modelMapper.map(userDto, User.class);
         String password = user.getPassword();
         if (password.equals(currentPassword)) {
             user.setPassword(newPassword);
             System.out.println("your password change successfully.");
-            return update(user);
+            UserDto newUserDto = modelMapper.map(user, UserDto.class);
+            update(newUserDto);
         } else {
             throw new InCorrectException("password is wrong!");
         }
