9c349349bc64d1e4c0520f2e7b5d8df7587e7511
nasim zamani <zamaninasim213@gmail.com>
2022-01-23 21:02:07 +0330

    add @ExceptionHandler in CustomerController

--- src/main/java/ir/maktab/web/CustomerController.java
+++ src/main/java/ir/maktab/web/CustomerController.java
@@ -1,18 +1,23 @@
 package ir.maktab.web;
 
+import ir.maktab.configuration.LastViewInterceptor;
 import ir.maktab.data.model.enumeration.Role;
 import ir.maktab.data.model.enumeration.UserStatus;
 import ir.maktab.dto.CustomerDto;
 import ir.maktab.service.CustomerService;
+import ir.maktab.service.exception.CustomerIsExistException;
 import ir.maktab.service.validation.OnRegister;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,5 +37,20 @@ public class CustomerController {
         customerDto.setRole(Role.CUSTOMER);
         customerService.save(customerDto);
         return "customer/profile";
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    public ModelAndView bindExceptionHandler(BindException ex, HttpServletRequest request) {
+        String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
+        return new ModelAndView(lastView, ex.getBindingResult().getModel());
+    }
+
+    //TODO
+    @ExceptionHandler(value = CustomerIsExistException.class)
+    public ModelAndView registerExceptionHandler(CustomerIsExistException ex) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("customer", new CustomerDto());
+        model.put("customerExist", ex.getMessage());
+        return new ModelAndView("customer/register", model);
     }
 }
