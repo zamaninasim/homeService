20e1692cd7befda2b605dadecf581749198ae336
nasim zamani <zamaninasim213@gmail.com>
2022-01-23 20:56:50 +0330

    add WebInitializer.java and delete Web.xml

---
+++ src/main/java/ir/maktab/configuration/WebInitializer.java
@@ -0,0 +1,28 @@
+package ir.maktab.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return null;
+    }
+}
