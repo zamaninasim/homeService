1d3c18ed34ad146d4c577073ec5dc554362a9588
nasim zamani <zamaninasim213@gmail.com>
2022-01-24 00:39:51 +0330

    modify CustomerDto.java

--- src/main/java/ir/maktab/dto/CustomerDto.java
+++ src/main/java/ir/maktab/dto/CustomerDto.java
@@ -4,11 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.util.List;
-
 @Data
 @SuperBuilder
 @NoArgsConstructor
 public class CustomerDto extends UserDto {
-    private List<OrderDto> orders;
 }
