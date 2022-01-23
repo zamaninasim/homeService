ee2b49a99849825608775cb01b2773c3783d5e5b
nasim zamani <zamaninasim213@gmail.com>
2022-01-24 00:37:06 +0330

    add findByEmailAddressAndPassword in ExpertRepsitory.java

--- src/main/java/ir/maktab/data/dao/ExpertRepository.java
+++ src/main/java/ir/maktab/data/dao/ExpertRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface ExpertRepository extends CrudRepository<Expert, Integer> {
     Optional<Expert> findByEmailAddress(String email);
+    Optional<Expert> findByEmailAddressAndPassword(String email,String password);
 }
