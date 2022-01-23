222d62664e66ef6bf160ff94e93746ab324b001d
nasim zamani <zamaninasim213@gmail.com>
2022-01-24 00:38:24 +0330

    add register method and handel exception in ExpertController

--- src/main/java/ir/maktab/web/ExpertController.java
+++ src/main/java/ir/maktab/web/ExpertController.java
@@ -1,14 +1,26 @@
 package ir.maktab.web;
 
+import ir.maktab.configuration.LastViewInterceptor;
+import ir.maktab.data.model.enumeration.Role;
+import ir.maktab.data.model.enumeration.UserStatus;
+import ir.maktab.dto.CustomerDto;
 import ir.maktab.dto.ExpertDto;
 import ir.maktab.service.ExpertService;
+import ir.maktab.service.exception.CustomerIsExistException;
+import ir.maktab.service.exception.ExpertIsExistException;
 import ir.maktab.service.validation.OnRegister;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,11 +35,27 @@ public class ExpertController {
 
     @PostMapping("/expertRegister")
     public String registerExpert(@ModelAttribute("expertDto") @Validated(OnRegister.class) ExpertDto expertDto,
-                           @RequestParam("image") CommonsMultipartFile image) {
+                                 @RequestParam("image") CommonsMultipartFile image) {
         expertDto.setPhoto(image.getBytes());
         expertDto.setScore(5.0);
-        //service
+        expertDto.setUserStatus(UserStatus.NEW);
+        expertDto.setCredit(0L);
+        expertDto.setRole(Role.EXPERT);
         expertService.save(expertDto);
         return "expert/profile";
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    public ModelAndView bindExceptionHandler(BindException ex, HttpServletRequest request) {
+        String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
+        return new ModelAndView(lastView, ex.getBindingResult().getModel());
+    }
+
+    @ExceptionHandler(value = ExpertIsExistException.class)
+    public ModelAndView registerExceptionHandler(ExpertIsExistException ex) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("expertDto", new CustomerDto());
+        model.put("error", ex.getMessage());
+        return new ModelAndView("expert/register", model);
     }
 }
