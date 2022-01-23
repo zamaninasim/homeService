31e4ff6f304fd96322ef79f3c04e82a289e6656e
nasim zamani <zamaninasim213@gmail.com>
2022-01-23 20:59:50 +0330

    add validation to UserDto

--- src/main/java/ir/maktab/dto/UserDto.java
+++ src/main/java/ir/maktab/dto/UserDto.java
@@ -2,6 +2,14 @@ package ir.maktab.dto;
 
 import ir.maktab.data.model.enumeration.Role;
 import ir.maktab.data.model.enumeration.UserStatus;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import ir.maktab.service.validation.OnLogin;
+import ir.maktab.service.validation.OnRegister;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +22,13 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+    @Pattern(regexp = "[a-zA-Z]{3,30}",message = "invalid firstname.", groups = {OnRegister.class})
     private String firstname;
+    @Pattern(regexp = "[a-zA-Z]{3,30}",message = "invalid lastname.", groups = {OnRegister.class})
     private String lastname;
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "Invalid email", groups = {OnLogin.class, OnRegister.class})
     private String emailAddress;
+    @Pattern(regexp ="^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",message = "Invalid password", groups = {OnLogin.class, OnRegister.class})
     private String password;
     private UserStatus userStatus;
     private Date registrationDate;
