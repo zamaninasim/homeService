58b885d3e9c78461dfd1ba744435296bcb86e48a
nasim zamani <zamaninasim213@gmail.com>
2022-01-23 20:53:48 +0330

    change Config.java to AppConfiguration.java

---
+++ src/main/java/ir/maktab/configuration/AppConfiguration.java
@@ -0,0 +1,28 @@
+package ir.maktab.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("ir.maktab")
+@PropertySource("classpath:database.properties")
+@EnableWebMvc
+public class AppConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver getResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        //set max upload size per file is 20mb
+        commonsMultipartResolver.setMaxUploadSizePerFile(300 * 1024*1024);
+        return commonsMultipartResolver;
+    }
+}
