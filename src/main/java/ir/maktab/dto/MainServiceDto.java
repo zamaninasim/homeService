ae28d2078900d68e80276cea6874ae1a00762708
nasim zamani <zamaninasim213@gmail.com>
2022-01-24 22:51:58 +0330

    add IdentificationNumber to MainServiceDto & SubServiceDto

--- src/main/java/ir/maktab/dto/MainServiceDto.java
+++ src/main/java/ir/maktab/dto/MainServiceDto.java
@@ -1,18 +1,19 @@
 package ir.maktab.dto;
 
+import ir.maktab.service.validation.OnRegister;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.HashSet;
-import java.util.Set;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MainServiceDto {
+    private Integer IdentificationNumber;
+    @Pattern(regexp = "[a-zA-Z]{3,30}", message = "invalid name.", groups = {OnRegister.class})
     private String name;
-    private Set<SubServiceDto> subServices = new HashSet<>();
 }
--- src/main/java/ir/maktab/dto/SubServiceDto.java
+++ src/main/java/ir/maktab/dto/SubServiceDto.java
@@ -5,17 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SubServiceDto {
+    private Integer IdentificationNumber;
     private String name;
-    private Long basePrice;
+    private String basePrice;
     private String description;
-    private MainServiceDto mainServiceDto;
-    private Set<ExpertDto> experts = new HashSet<>();
+    private String mainServiceName;
 }
