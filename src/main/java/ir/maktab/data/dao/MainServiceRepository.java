6fff1bc7f7566d7ff6a9e7744a76b4b296527a54
nasim zamani <zamaninasim213@gmail.com>
2022-01-24 15:11:23 +0330

    change MainServiceRepository extend

--- src/main/java/ir/maktab/data/dao/MainServiceRepository.java
+++ src/main/java/ir/maktab/data/dao/MainServiceRepository.java
@@ -1,12 +1,13 @@
 package ir.maktab.data.dao;
 
 import ir.maktab.data.model.entity.services.MainService;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MainServiceRepository extends CrudRepository<MainService, Integer> {
+public interface MainServiceRepository extends JpaRepository<MainService, Integer> {
     Optional<MainService> findByName(String name);
 }
